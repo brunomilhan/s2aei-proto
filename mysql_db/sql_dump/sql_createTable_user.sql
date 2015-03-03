c4a5b1bb6951d14d8dcc00ef960801b703ba53a1
brunomilhan <brunomilhan2@gmail.com>
2015-03-03 00:45:34 -0300

    change user column to login.

--- mysql_db/sql_dump/sql_createTable_user.sql
+++ mysql_db/sql_dump/sql_createTable_user.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `s2aei_beta_db`.`user` (
   `iduser` INT NOT NULL AUTO_INCREMENT,
-  `user` VARCHAR(45) NOT NULL,
+  `login` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `permission` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`iduser`),
