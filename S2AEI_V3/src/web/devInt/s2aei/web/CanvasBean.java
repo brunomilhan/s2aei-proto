4ff641608dc91d1a2259bf300105cfeb4ed10652
brunomilhan <brunomilhan2@gmail.com>
2015-03-04 15:15:45 -0300

    some changes before deploy. (need change this msg)

--- S2AEI_V3/src/web/devInt/s2aei/web/CanvasBean.java
+++ S2AEI_V3/src/web/devInt/s2aei/web/CanvasBean.java
@@ -9,7 +9,6 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import devInt.s2aei.canvas.CanvasBR;
-import devInt.s2aei.canvas.CanvasQuestions;
 import devInt.s2aei.canvas.QuestionCanvas;
 import devInt.s2aei.project.Project;
 import devInt.s2aei.project.ProjectBR;
@@ -19,28 +18,28 @@ import devInt.s2aei.student.StudentBR;
 @ManagedBean
 @RequestScoped
 public class CanvasBean {
-	
+
 	private CanvasBR canvasBR = new CanvasBR();
-	
-	//private QuestionCanvas questionCanvas = new QuestionCanvas();
+
+	// private QuestionCanvas questionCanvas = new QuestionCanvas();
 	private Project project;
 	private ProjectBR projectBR = new ProjectBR();
-	
+
 	private List<QuestionCanvas> questionsCanvas;
 	private Student student;
 	private StudentBR studentBR = new StudentBR();
-	
-	
+
 	@PostConstruct
-	public void construct(){
-		//this.questionsCanvas = this.canvasBR.listByProject(project);
-		this.student = this.studentBR.findStudentByLogin(SessionBean.getLoginUserLogged());
+	public void construct() {
+		// this.questionsCanvas = this.canvasBR.listByProject(project);
+		this.student = this.studentBR.findStudentByLogin(SessionBean
+				.getLoginUserLogged());
 		this.project = this.projectBR.findProjectActiveByStudent(student);
-		
+
 		this.questionsCanvas = this.canvasBR.listByProject(project);
 	}
-	
-	public List<QuestionCanvas> listAll(){
+
+	public List<QuestionCanvas> listAll() {
 		return this.questionsCanvas;
 	}
 
@@ -51,20 +50,26 @@ public class CanvasBean {
 	public void setQuestionsCanvas(List<QuestionCanvas> questionsCanvas) {
 		this.questionsCanvas = questionsCanvas;
 	}
-	
-	public void saveQuest(){
+
+	public void saveQuest() {
 		try {
-			for (QuestionCanvas questionCanvas : questionsCanvas) {
-				this.canvasBR.saveQuest(questionCanvas);
-			}			
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage("Pergunta respondida com sucesso!"));
+			if (this.project.getLeader() == this.student) {
+				for (QuestionCanvas questionCanvas : questionsCanvas) {
+					this.canvasBR.saveQuest(questionCanvas);
+				}
+				FacesContext.getCurrentInstance().addMessage(null,
+						new FacesMessage("Pergunta respondida com sucesso!"));
+			} else {
+				FacesContext.getCurrentInstance().addMessage(null,
+						new FacesMessage("Somente o aluno líder pode responder as perguntas."));
+			}
 		} catch (Exception e) {
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage("Não foi possivel atualizar tente novamente."));
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(
+							"Não foi possivel atualizar tente novamente."));
 		}
-		
-		
+
 	}
 
 }
